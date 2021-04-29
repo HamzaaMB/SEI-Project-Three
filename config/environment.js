37836ece3a5d4a2f3e962f639bddf263181bfe82
Hamza Butt <hamzambutt1@gmail.com>
2021-04-29 14:52:17 +0100

    heroku push

--- config/environment.js
+++
@@ -1,7 +0,0 @@
-export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/icelander'
-export const port = process.env.PORT || 4000
-export const secret = process.env.SECRET || 'This is a secret'
-
-
-
-
